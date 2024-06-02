e06649e027078f6285f78c6406d7c5a01a071330
M.Ajwad <72156399+DeCryptAF@users.noreply.github.com>
2024-06-02 23:43:31 +0600

    Update struct_customer_account.c

--- STRUCT & UNION/struct_customer_account.c
+++ STRUCT & UNION/struct_customer_account.c
@@ -2,43 +2,40 @@
 Problem: Write a C program that takes customer data from the user and prints it.
 It will have a First Name, Last Name, Account Number, Balance.
 */
+
 #include <stdio.h>
 
-struct account
-{
-  //nested struct for name
-	struct 
-	{
-		char firstName[10];
-		char lastName[10];
-	} names;
-	int accountNum;
-	double balance;
+struct account {
+    struct {
+        char firstName[10];
+        char lastName[10];
+    } names;
+    int accountNum;
+    double balance;
 };
 
-//funtion declaration
+// Function prototypes
 void customer_info(struct account *acct);
 void print_customer(struct account acct);
 
-int main()
-{
-	struct account record;
-	int n;
-	printf("Enter the number of accounts: ");
-	scanf("%d", &n);
+int main() {
+    struct account record;
+    int n;
+    printf("Enter the number of accounts: ");
+    scanf("%d", &n);
 
-	for (int i = 0; i < n; i++)
-	{
-		printf("\nACCOUNT :\n");
-		customer_info(&record);
-		print_customer(record);
-	}
-	return 0;
+    for (int i = 0; i < n; i++) {
+        printf("\nACCOUNT %d:\n", i + 1);
+        customer_info(&record);
+        print_customer(record);
+    }
+
+    return 0;
 }
 
-void customer_info(struct account *acct)
-{
-	printf("Enter first name: ");
+// Function to read customer information
+void customer_info(struct account *acct) {
+    printf("Enter first name: ");
     scanf("%s", acct->names.firstName);
     printf("Enter last name: ");
     scanf("%s", acct->names.lastName);
@@ -48,11 +45,12 @@ void customer_info(struct account *acct)
     scanf("%lf", &acct->balance);
 }
 
-void print_customer(struct account acct)
-{
-	printf("\nCustomer Information:\n");
+// Function to print customer information
+void print_customer(struct account acct) {
+    printf("\nCustomer Information:\n");
     printf("First Name: %s\n", acct.names.firstName);
     printf("Last Name: %s\n", acct.names.lastName);
     printf("Account Number: %d\n", acct.accountNum);
     printf("Balance: %.2lf\n\n", acct.balance);
 }
+
